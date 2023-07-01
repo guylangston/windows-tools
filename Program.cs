67cafb342508b242205d7a427ff3e6fa863a2a88
Guy Langston <guylangston@hotmail.com>
2023-07-01 14:02:08 +0100

    feat: added process info

--- Program.cs
+++ Program.cs
@@ -1,81 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.Runtime.InteropServices;
-using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-
-public static class Windows
-{
-    public delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
-
-    [DllImport("user32.dll")] public static extern bool   EnumWindows(EnumWindowsProc enumProc, IntPtr lParam);
-    [DllImport("user32.dll")] public static extern int    GetWindowText(IntPtr hWnd, System.Text.StringBuilder lpString, int nMaxCount);
-    [DllImport("user32.dll")] public static extern bool   IsWindowVisible(IntPtr hWnd);
-    [DllImport("user32.dll")] public static extern bool   SetForegroundWindow(IntPtr hWnd);
-    [DllImport("user32.dll")] public static extern int    SendMessage(IntPtr hWnd, uint Msg, int wParam, int lParam);
-    [DllImport("user32.dll")] public static extern IntPtr GetForegroundWindow();
-
-    [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
-    static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName,int nMaxCount);
-
-    const uint WM_KEYDOWN = 0x0100;
-    const uint WM_KEYUP = 0x0101;
-
-    public static void SendKey(IntPtr windowHandle, int key)
-    {
-        SendMessage(windowHandle, WM_KEYDOWN, (int)key, 0);
-        SendMessage(windowHandle, WM_KEYUP, (int)key, 0);
-    }
-
-    private static readonly object lockerSendKeys = new();
-    public static void SendKeys(IntPtr windowHandle, int key)
-    {
-        lock(lockerSendKeys)
-        {
-            if (SetForegroundWindow(windowHandle))
-            {
-                SendKey(windowHandle, key);
-            }
-        }
-    }
-
-    public static string? GetWindowClassName(IntPtr hWin)
-    {
-        // Pre-allocate 256 characters, since this is the maximum class name length.
-        var className = new StringBuilder(256);
-        //Get the window class name
-        if (GetClassName(hWin, className, className.Capacity) == 0) return null;
-        return className.ToString();
-    }
-
-    public record WindowInfo(IntPtr Handle, string Title, string? WindowClass);
-
-    public record WindowInfoReport(uint Handle, string Title, string? WindowClass)
-    {
-        public WindowInfoReport(WindowInfo info) : this((uint)info.Handle, info.Title, info.WindowClass) { }
-    };
-
-    public static List<WindowInfo> EnumWindowsWrapper()
-    {
-        var windowHandles = new List<WindowInfo>();
-        EnumWindows( (IntPtr hWnd, IntPtr lParam) => 
-        {
-            if (IsWindowVisible(hWnd))
-            {
-                var sb = new StringBuilder(1024);
-                GetWindowText(hWnd, sb, sb.Capacity);
-                var title = sb.ToString();
-                if (title.Length > 0) 
-                {
-                    windowHandles.Add( new WindowInfo(hWnd, title, GetWindowClassName(hWnd)));
-                }
-            }
-            return true;
-        }, IntPtr.Zero);
-        return windowHandles;
-    }
-}
 
 static class Program
 {
@@ -84,19 +10,19 @@ static class Program
         var json = !args.Contains("--text");
         var refreshChrome = args.Contains("--refresh-chrome");
 
-        var windows = Windows.EnumWindowsWrapper().OrderBy(x => x.Handle).ThenBy(x => x.Title).ToList();
+        var windows = Windows.EnumWindowsWrapper().OrderBy(x => x.HandleIntPtr).ThenBy(x => x.Title).ToList();
 
         if (refreshChrome)
         {
             var curr = Windows.GetForegroundWindow();
             foreach (var handle in windows)
             {
-                Console.WriteLine($"{handle.Handle,12} {Windows.GetWindowClassName(handle.Handle)}:{handle.Title}");
+                Console.WriteLine($"{handle.HandleIntPtr,12} {Windows.GetWindowClassName(handle.HandleIntPtr)}:{handle.Title}");
                 if (args.Length > 0 && args.Contains("--chrome-refresh"))
                 {
                     if (handle.Title.EndsWith("Google Chrome"))
                     {
-                        Windows.SendKeys(handle.Handle, 0x74); //  https://learn.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
+                        Windows.SendKeys(handle.HandleIntPtr, 0x74); //  https://learn.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
                         Console.WriteLine(" --> send: F5");
                     }
                 }
@@ -110,7 +36,7 @@ static class Program
 
         if (json)
         {
-            Console.WriteLine(JsonSerializer.Serialize(windows.Select(x=>new Windows.WindowInfoReport(x)), new JsonSerializerOptions()
+            Console.WriteLine(JsonSerializer.Serialize(windows, new JsonSerializerOptions()
             {
                 WriteIndented = true
             }));
@@ -121,7 +47,7 @@ static class Program
         {
             foreach (var handle in windows)
             {
-                Console.WriteLine($"{handle.Handle,12} {Windows.GetWindowClassName(handle.Handle)}:{handle.Title}");
+                Console.WriteLine($"{handle.HandleIntPtr,12} {Windows.GetWindowClassName(handle.HandleIntPtr)}:{handle.Title}");
             }
 
             return 0;
---
+++ Windows.cs
@@ -0,0 +1,99 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.Json.Serialization;
+
+public static class Windows
+{
+    public delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+
+    [DllImport("user32.dll")] public static extern bool   EnumWindows(EnumWindowsProc enumProc, IntPtr lParam);
+    [DllImport("user32.dll")] public static extern int    GetWindowText(IntPtr hWnd, System.Text.StringBuilder lpString, int nMaxCount);
+    [DllImport("user32.dll")] public static extern bool   IsWindowVisible(IntPtr hWnd);
+    [DllImport("user32.dll")] public static extern bool   SetForegroundWindow(IntPtr hWnd);
+    [DllImport("user32.dll")] public static extern int    SendMessage(IntPtr hWnd, uint Msg, int wParam, int lParam);
+    [DllImport("user32.dll")] public static extern IntPtr GetForegroundWindow();
+
+    [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+    static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName,int nMaxCount);
+    
+    [DllImport("user32.dll", SetLastError=true)]
+    static extern uint GetWindowThreadProcessId(IntPtr hWnd, out int processId);
+    
+
+    const uint WM_KEYDOWN = 0x0100;
+    const uint WM_KEYUP = 0x0101;
+
+    public static void SendKey(IntPtr windowHandle, int key)
+    {
+        SendMessage(windowHandle, WM_KEYDOWN, (int)key, 0);
+        SendMessage(windowHandle, WM_KEYUP, (int)key, 0);
+    }
+
+    private static readonly object lockerSendKeys = new();
+    public static void SendKeys(IntPtr windowHandle, int key)
+    {
+        lock(lockerSendKeys)
+        {
+            if (SetForegroundWindow(windowHandle))
+            {
+                SendKey(windowHandle, key);
+            }
+        }
+    }
+
+    public static string? GetWindowClassName(IntPtr hWin)
+    {
+        // Pre-allocate 256 characters, since this is the maximum class name length.
+        var className = new StringBuilder(256);
+        //Get the window class name
+        if (GetClassName(hWin, className, className.Capacity) == 0) return null;
+        return className.ToString();
+    }
+
+    public record WindowInfo([property: JsonIgnore]IntPtr HandleIntPtr, 
+        string Title, 
+        string? WindowClass,
+        int ProcessId,
+        [property: JsonIgnore]Process Process)
+    {
+        public uint Handle => (uint)HandleIntPtr;
+        public string? ProcessName => Process?.ProcessName;
+        public DateTime? Started => Process?.StartTime;
+        
+        // https://stackoverflow.com/questions/2633628/can-i-get-command-line-arguments-of-other-processes-from-net-c
+        // NOTE: Cannot use StartInfo for an unrelated process (not started by this process)
+        // public string? ProcessCmd => Process?.StartInfo.FileName;
+    }
+
+   
+
+    public static List<WindowInfo> EnumWindowsWrapper()
+    {
+        var windowHandles = new List<WindowInfo>();
+        EnumWindows( (hWnd, _) => 
+        {
+            if (IsWindowVisible(hWnd))
+            {
+                var sb = new StringBuilder(1024);
+                if (GetWindowText(hWnd, sb, sb.Capacity) != 0)
+                {
+                    var title = sb.ToString();
+                    if (title.Length > 0)
+                    {
+                        // Get the Window Handle's Process
+                        Process? proc = null;
+                        if (GetWindowThreadProcessId(hWnd, out var procId) != 0)
+                        {
+                            proc = Process.GetProcessById(procId);
+                        }
+
+                        windowHandles.Add( new WindowInfo(hWnd, title, GetWindowClassName(hWnd), procId, proc));
+                    }
+                }
+            }
+            return true;
+        }, IntPtr.Zero);
+        return windowHandles;
+    }
+}
